fa5fa7161b4b322dceebb925aa99bcc4de9b92d4
evaserman <ellieav@mit.edu>
2022-03-04 01:24:42 -0500

    commit

--- 7.10.sql
+++ 7.10.sql
@@ -1,8 +1,9 @@
 -- Books in an Order
 SELECT 
 	Books.Title,
-    COUNT(*) AS AmountOrdered
+    COUNT(Orders.BookID) AS AmountOrdered
 FROM Orders
 JOIN Books
 ON Books.BookID = Orders.BookID
 GROUP BY Books.BookID
+HAVING COUNT(Orders.BookID)>1
