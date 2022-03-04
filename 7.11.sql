a6d6ffe814a87bfc911a9f841ad43f97cfe4735b
evaserman <ellieav@mit.edu>
2022-03-04 01:22:23 -0500

    commit

--- 7.11.sql
+++ 7.11.sql
@@ -1,9 +1,14 @@
 -- Customer Orders
 SELECT 
--- Customers.CustomerFirstName, 
--- Customers.CustomerLastName,
 Orders.OrderID,
-COUNT(DISTINCT Orders.OrderID) AS NumberofBooks
-FROM Customers 
-LEFT JOIN Orders
+Orders.BookID,
+Books.Title,
+Customers.CustomerFirstName,
+Customers.CustomerLastName
+FROM 
+Orders
+JOIN Books
+ON Books.BookID = Orders.BookID
+JOIN Customers
 ON Customers.CustomerID = Orders.CustomerID
+ORDER BY OrderID ASC
